ce14e444f7644e621c7414b54b0388945bb5c225
brettmadrid <brettmadrid9@gmail.com>
2021-06-11 13:56:13 -0700

    added another get to /hello endpoint

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ server.get('/', (req, res) => {
     res.json({ Hello: "world!"});
 });
 
+server.get('/hello', (req, res) => {
+    res.json({ hello: "sexy"});
+});
+
 server.listen(PORT, () => {
     console.log(`*** Server running on port ${PORT} ***`)
 });
